bd92cb3717eda483aae30685abf8b6d71b052ce0
Noziho <noah.decroix3@gmail.com>
2023-01-10 15:37:11 +0100

    done

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Post It !</title>
+</head>
+<body>
+    <header>
+        <h1><a href="http://localhost:8000">Post It !</a></h1>
+    </header>
+
+    <div class="container">
+        <div id="postItContainer">
+            <textarea name="postIt" id="postIt" cols="30" rows="10" placeholder="Post It !"></textarea>
+        </div>
+    </div>
+</body>
+</html>
